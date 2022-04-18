412208f78b1b276e562a4bf9340116b112687ee2
Yasser Hajlaoui <yasser159@gmail.com>
2022-04-17 23:05:58 -0700

    init

--- Ui making room for Keyboard/ViewController.swift
+++ Ui making room for Keyboard/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var myTextBox: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
 
+    
+ }
 
 }
-
